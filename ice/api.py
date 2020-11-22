6de0bc7b6f19ca440927827fe8b2e82863e49ea2
Marius Widmann <marius.widmann@gmail.com>
2020-11-22 20:50:14 +0000

    Added syncperiod to event

--- ice/api.py
+++ ice/api.py
@@ -11,22 +11,6 @@ import dateutil
 import re
 from ice.caldav_utils import *
 
-def cleanName(name):
-    name = name.replace("<","")
-    name = name.replace(">","")
-
-    return name
-
-def color_variant(hex_color, brightness_offset=50):
-    """ takes a color like #87c95f and produces a lighter or darker variant """
-    if len(hex_color) != 7:
-        raise Exception("Passed %s into color_variant(), needs to be in #87c95f format." % hex_color)
-    rgb_hex = [hex_color[x:x+2] for x in [1, 3, 5]]
-    new_rgb_int = [int(hex_value, 16) + brightness_offset for hex_value in rgb_hex]
-    new_rgb_int = [min([255, max([0, i])]) for i in new_rgb_int] # make sure new values are between 0 and 255
-    # hex() produces "0x88", we want just "88"
-    return "#" + "".join([hex(i)[2:] for i in new_rgb_int])
-
 @frappe.whitelist()
 def fetch_calendars(data):
     #Check if called from client side (not necessary)
--- ice/caldav_utils.py
+++ ice/caldav_utils.py
@@ -7,6 +7,23 @@ sys.path.insert(0, '..')
 import dateutil
 import re
 
+def cleanName(name):
+    name = name.replace("<","")
+    name = name.replace(">","")
+
+    return name
+
+def color_variant(hex_color, brightness_offset=50):
+    """ takes a color like #87c95f and produces a lighter or darker variant """
+    if len(hex_color) != 7:
+        raise Exception("Passed %s into color_variant(), needs to be in #87c95f format." % hex_color)
+    rgb_hex = [hex_color[x:x+2] for x in [1, 3, 5]]
+    new_rgb_int = [int(hex_value, 16) + brightness_offset for hex_value in rgb_hex]
+    new_rgb_int = [min([255, max([0, i])]) for i in new_rgb_int] # make sure new values are between 0 and 255
+    # hex() produces "0x88", we want just "88"
+    return "#" + "".join([hex(i)[2:] for i in new_rgb_int])
+
+
 def isMappable(vev, rule):
     #Ausschluss EXDATE, EXRULE, RDATE (https://www.kanzaki.com/docs/ical/)
     if(hasattr(vev,"exdate") or hasattr(vev,"rdate") or hasattr(vev,"exrule")):
